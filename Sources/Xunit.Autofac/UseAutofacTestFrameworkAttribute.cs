122bfb36fcf340495fb8553084cfa8c3ce7a011d
Sergey Rogovtsev <srogovtsev@acumatica.com>
2017-07-26 18:21:52 +0300

    Added simplest possible test project to show usage
    * also added explicit attribute to simplify referencing the framework from tests

---
+++ Sources/Xunit.Autofac/UseAutofacTestFrameworkAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Xunit.Autofac
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    [TestFrameworkDiscoverer("Xunit.Autofac." + nameof(AutofacTestFrameworkTypeDiscoverer), "Xunit.Autofac")]
+    public class UseAutofacTestFrameworkAttribute: Attribute, ITestFrameworkAttribute
+    {
+    }
+
+    internal class AutofacTestFrameworkTypeDiscoverer: ITestFrameworkTypeDiscoverer
+    {
+        public Type GetTestFrameworkType(IAttributeInfo attribute) => typeof(AutofacTestFramework);
+    }
+}
---
+++ Tests/Xunit.Autofac.Usage.Tests/SimpleTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Xunit.Autofac.Usage.Tests
+{
+    public class SimpleTests
+    {
+        [Fact]
+        public void Fact()
+        {
+            
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        public void Theory(int no)
+        {
+            
+        }
+    }
+}
